b90bdf39994168453f1deb32a04ec3a3506853ae
Al Davidson <apdavidson@gmail.com>
2019-03-12 10:31:25 +0000

    added specs for recently-added methods

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -16,4 +16,34 @@ describe Person do
       end
     end
   end
+
+  context 'when a person has roles' do
+    let(:role_1) { build(:role, title: 'Chief Executive') }
+    let(:role_2) { build(:role, title: 'Head of Personnel') }
+
+    before do
+      subject.roles << role_1
+      subject.roles << role_2
+    end
+
+    describe '#role_titles' do
+      it 'returns an array of all the role titles' do
+        expect(subject.role_titles).to eq(['Chief Executive', 'Head of Personnel'])
+      end
+    end
+
+    context '#organisation_names' do
+      let(:org_1) { build(:organisation, name: 'ACME Construction') }
+      let(:org_2) { build(:organisation, name: 'Barraclough Leisure') }
+
+      before do
+        role_1.organisation = org_1
+        role_2.organisation = org_2
+      end
+
+      it 'returns the names of the organisations in which they have roles' do
+        expect(subject.organisation_names).to eq(['ACME Construction', 'Barraclough Leisure'])
+      end
+    end
+  end
 end
